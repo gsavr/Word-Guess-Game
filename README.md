dfb4ab32a806bad813aeec90d0a4f992749b6504
gsavr <giorgiosav@me.com>
2018-12-09 20:01:49 -0500

    js game start

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Word-Guess-Game
+
+var with all answers
+make a variable to choose random word
+make a variable to split word into each indvidual letter
+make a variable to make blank underscore for each letter
+make a variable where each incorrect letter goes
+
+var for win
+var for turns left
+
+function#1
+when site loads, random word is chosen,
+
+then word is split into letters and displayed on screen as blanks
+
+
+
+if the correct letter was chosen, then it is diplayed on the correct blank where it goes
+
+if the incorrect letter is chosen, then it is displayed on the wrong letter pile, and
+turns left goes down by 1
+
+once whole word is filled in, then you win is displayed with a picture of the answer and a sound plays
+
+user presses key to go back to play again
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,41 @@
-var words = ["micrsocope", ""]
+var words = ["microscope", "laboratory", "science", "atom", "brain", "heart", "mitochondria", "cells", "organelle", "organ", "erosion", "weathering", "conduction", "convection", "radiation"];
+
+// to use for storing random answer
+var wordAnswer = "";
+//will split with random answer into an array with each letter as a value
+var answerLetters = [];
+
+// will count the number of blanks that will be shown
+var numberSpaces = 0 ;
+var blanks = []
+//will store array of incorrect answers
+var wrongLetter= [];
+//do I need another var?
+//will store number of wins
+var win = 0;
+//shows turns left in game
+var turnsLeft = 10;
+
+function Start(){
+    wordAnswer = words[Math.floor(Math.random() * words.length)];
+
+    answerLetters = wordAnswer.split("");
+
+    numberSpaces = wordAnswer.length;
+
+    // create loop to change element id to blanks
+    for (var i=0; i < numberSpaces; i++){
+        blanks.push("_");
+        document.getElementById("currentWord").innerHTML = "  " + blanks.join("");
+    }
+console.log(wordAnswer);
+console.log(answerLetters);
+console.log(numberSpaces);
+console.log(blanks.join(" "))
+}
+
+Start()
+
+document.onkeydown = function(event) {
+
+}
