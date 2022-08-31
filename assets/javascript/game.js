c3e9fe87d5cf35ffccd148f31f9e8478e64d7984
giorgio <giorgiosavron@me.com>
2022-08-31 16:06:59 -0400

    iOS keyboard

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,20 @@
-var words = ["microscope", "laboratory", "science", "atom", "brain", "heart", "mitochondria", "cells", "organelle", "organ", "erosion", "weathering", "conduction", "convection", "radiation"];
+var words = [
+  "microscope",
+  "laboratory",
+  "science",
+  "atom",
+  "brain",
+  "heart",
+  "mitochondria",
+  "cells",
+  "organelle",
+  "organ",
+  "erosion",
+  "weathering",
+  "conduction",
+  "convection",
+  "radiation",
+];
 
 // to use for storing random answer
 var wordAnswer = "";
@@ -6,10 +22,10 @@ var wordAnswer = "";
 var answerLettersArr = [];
 
 // will count the number of blanks that will be shown
-var numberSpaces = 0 ;
-var blanks = [numberSpaces]
+var numberSpaces = 0;
+var blanks = [numberSpaces];
 //will store incorrect answers
-var badLetter= [];
+var badLetter = [];
 //do I need another var?
 //will store number of wins
 var win = 0;
@@ -20,92 +36,81 @@ var output = "";
 
 var userChoice = ""; //key event
 
-function Start(){
-    output = "";
-    document.getElementById("alreadyGuessed").innerHTML = "";
-    turnsLeft=15;
-    wordAnswer = words[Math.floor(Math.random() * words.length)];
+function Start() {
+  output = "";
+  document.getElementById("alreadyGuessed").innerHTML = "";
+  turnsLeft = 15;
+  wordAnswer = words[Math.floor(Math.random() * words.length)];
 
-    answerLettersArr = wordAnswer.split("");
+  answerLettersArr = wordAnswer.split("");
 
-    numberSpaces = answerLettersArr.length;
+  numberSpaces = answerLettersArr.length;
 
-    // create loop to change element id to blanks
-    for (var i=0; i < numberSpaces; i++)
-    {
-        blanks[i]=" _";
-        output = output + blanks[i];
-    }
-    document.getElementById("currentWord").innerHTML =  output;
-    output = "";
-    
-console.log(wordAnswer);
-console.log(answerLettersArr);
-console.log(numberSpaces);
+  // create loop to change element id to blanks
+  for (var i = 0; i < numberSpaces; i++) {
+    blanks[i] = " _";
+    output = output + blanks[i];
+  }
+  document.getElementById("currentWord").innerHTML = output;
+  output = "";
 
+  console.log(wordAnswer);
+  console.log(answerLettersArr);
+  console.log(numberSpaces);
 }
 
-Start()
-
-
+Start();
 
 //function to check whether key pressed is correct or not
-document.onkeydown = function(event){
-    var userChoice = event.key;
-    
-    output = "";
-    for (var z = 0; z < answerLettersArr.length; z++) 
-    {
-          
-        if(userChoice == answerLettersArr[z])
-        { 
-            console.log("you got a letter! " +  answerLettersArr[z] );
+document.onkeydown = function (event) {
+  var userChoice = event.key;
+  console.log(userChoice);
 
-            blanks[z] = userChoice;
-            numberSpaces--;
-            console.log(userChoice);
-            console.log(answerLettersArr[z])
-        }
-        output = output + blanks[z] + " ";
-        
+  output = "";
+  for (var z = 0; z < answerLettersArr.length; z++) {
+    if (userChoice === answerLettersArr[z]) {
+      //console.log("you got a letter! " + answerLettersArr[z]);
+
+      blanks[z] = userChoice;
+      numberSpaces--;
+      //   console.log(userChoice);
+      //   console.log(answerLettersArr[z]);
     }
-    if(!wordAnswer.includes(userChoice))
-        {   
-            turnsLeft--;
-            document.getElementById("numberGuesses").innerHTML = turnsLeft;
-            //document.getElementById("alreadyGuessed").innerHTML = userChoice;
-            badChoice()
-            
-        }
-    
-    document.getElementById("currentWord").innerHTML = output;
-    
+    output = output + blanks[z] + " ";
+  }
+  if (!wordAnswer.includes(userChoice) && userChoice !== "Backspace") {
+    turnsLeft--;
+    document.getElementById("numberGuesses").innerHTML = turnsLeft;
+    //document.getElementById("alreadyGuessed").innerHTML = userChoice;
+    badChoice();
+  }
 
-    console.log(turnsLeft);
-    console.log("spaces left: " +numberSpaces);
+  document.getElementById("currentWord").innerHTML = output;
 
-    if (turnsLeft <1)
-    {
-    alert("You lose, the word was " + wordAnswer)
+  //   console.log(turnsLeft);
+  //   console.log("spaces left: " + numberSpaces);
+
+  if (turnsLeft < 1) {
+    alert("You lose, the word was " + wordAnswer);
     Start();
-    }
+  }
 
-    if (numberSpaces === 0)
-    {
-        win++;
-        document.getElementById("win").innerHTML = win;
-        setTimeout(function() {
-            alert("you win! The word was " + wordAnswer);
-          }, 500);
-        setTimeout(function() {
-           Start();}, 500);
-    }
-    console.log("wins : " + win);
-    
-    function badChoice(){
-        var node = document.createElement("span");
-        var badLetter = document.createTextNode(userChoice);
-        node.appendChild(badLetter);
-        document.getElementById("alreadyGuessed").appendChild(node);}
-}
+  if (numberSpaces === 0) {
+    win++;
+    document.getElementById("win").innerHTML = win;
+    setTimeout(function () {
+      alert("you win! The word was " + wordAnswer);
+    }, 500);
+    setTimeout(function () {
+      Start();
+    }, 500);
+  }
+  //   console.log("wins : " + win);
 
+  function badChoice() {
+    var node = document.createElement("span");
+    var badLetter = document.createTextNode(userChoice);
+    node.appendChild(badLetter);
+    document.getElementById("alreadyGuessed").appendChild(node);
+  }
+};
